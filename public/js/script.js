9379074c06cf405f4c3afe5e4e822b8ca08e7302
YON <yoncise@qq.com>
2022-04-04 22:06:37 +0800

    fix:toggle style

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,20 @@
 (function(document) {
-  var toggle = document.querySelector('.sidebar-toggle');
+  var toggle = document.querySelector('#sidebar-toggle');
+  var rightToggle = document.querySelector('#right-sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
+  var rightSidebar = document.querySelector('#right-sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  var rightCheckbox = document.querySelector('#right-sidebar-checkbox');
 
   document.addEventListener('click', function(e) {
     var target = e.target;
 
-    if(!checkbox.checked ||
+    if((!checkbox.checked && !rightCheckbox.checked) ||
        sidebar.contains(target) ||
-       (target === checkbox || target === toggle)) return;
+       rightSidebar.contains(target) ||      
+       target === checkbox || target === toggle || target === rightCheckbox || target === rightToggle) return;
 
     checkbox.checked = false;
+    rightCheckbox.checked = false;
   }, false);
 })(document);
